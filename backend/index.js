9b4603bfc08be0489e1b8bb03d9543e1f2105060
CoKhanh <duongcokhanh17110315@gmail.com>
2021-11-09 22:36:58 +0700

    [HW] Done

---
+++ backend/index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const cors = require("cors"); 
+
+app.use(cors());
+
+app.get("/",(req, res) => {
+    const response = [
+        {
+            id: 1,
+            name: 'Nguyen Van A',
+            age: 22,
+            salary: '1000$',
+            sex: '0'
+        },
+        {
+            id: 2,
+            name: 'Nguyen Van B',
+            age: 23,
+            salary: '1500$',
+            sex: '0'
+        },
+        {
+            id: 3,
+            name: 'Nguyen Van C',
+            age: 24,
+            salary: '2000$',
+            sex: '1'
+        },
+        {
+            id: 4,
+            name: 'Nguyen Van D',
+            age: 22,
+            salary: '1000$',
+            sex: '0'
+        },
+    ]
+    res.json(response)
+});
+
+server.listen(5000, () => {
+    console.log('Server start at 5000')
+});
---
+++ frontend/hello-world/src/App.js
@@ -0,0 +1,103 @@
+import { Fragment, useEffect, useState } from 'react';
+import './App.css';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { Box } from '@mui/system';
+import { Checkbox, Typography } from '@mui/material';
+
+function App() {
+  const url = 'http://localhost:5000/';
+  const [data, setData] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(url);
+      return await response.json();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchData().then((res) => {
+      setData(res)
+    });
+  }, [])
+
+  return (
+    <Box style={{ backgroundColor: '#173A5E', height: '100vh' }}>
+      <Box style={{ textAlign: "center" }}>
+        <Typography variant="h4" color="white">
+          Hello World
+        </Typography>
+      </Box>
+      {data && data.length > 0 ?
+        <Box style={{ padding: "2rem" }}>
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>
+                    <strong>Name</strong>
+                  </TableCell>
+                  <TableCell align="right">
+                    <strong>Age</strong>
+                  </TableCell>
+                  <TableCell align="right">
+                    <Box style={{
+                      display: "flex",
+                      flexDirection: "column"
+                    }}>
+                      <Box>
+                        <strong>Sex</strong>
+                      </Box>
+                      <Box>
+                        <Typography variant="caption">Checked is male</Typography>
+                      </Box>
+                    </Box>
+                  </TableCell>
+                  <TableCell align="right">
+                    <strong>Salary</strong>
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {data.map((item) => (
+                  <TableRow
+                    key={item.id}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {item.name}
+                    </TableCell>
+                    <TableCell align="right">{item.age}</TableCell>
+                    <TableCell align="right">
+                      {+item.sex === 0 ? 
+                        <Fragment>
+                          <Checkbox checked disabled/>
+                        </Fragment>
+                      : 
+                        <Fragment>
+                          <Checkbox disabled/>
+                        </Fragment>}
+                    </TableCell>
+                    <TableCell align="right">{item.salary}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Box>
+        : 
+        null
+      }
+    </Box>
+  );
+}
+
+export default App;
