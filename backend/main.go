9ffe96b6f0841bb8a09efc8106c1cc64a45c61d1
DESKTOP-SEALL29\Razer Stealth 13 <phngkhuongduy@gmail.com>
2021-11-10 10:02:43 +0700

    Call API from localhost

--- backend/main.go
+++ backend/main.go
@@ -3,10 +3,14 @@ package main
 import (
  "backend/controller"
  "github.com/labstack/echo/v4"
+ "github.com/labstack/echo/v4/middleware"
 )
 func main() {
  server := echo.New()
-
+ server.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+  AllowOrigins: []string{"*"},
+  AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+ }))
  general := server.Group("/general")
  {
   general.GET("/hello", controller.Hello)
