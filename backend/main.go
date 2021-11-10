f83c147ea44c6005a8181f57d6fb88d6c34acfcb
DESKTOP-SEALL29\Razer Stealth 13 <phngkhuongduy@gmail.com>
2021-11-10 21:50:17 +0700

    config heroku

--- backend/main.go
+++ backend/main.go
@@ -4,18 +4,18 @@ import (
  "backend/controller"
  "github.com/labstack/echo/v4"
  "github.com/labstack/echo/v4/middleware"
+ "os"
 )
 func main() {
+ port := os.Getenv("PORT")
  server := echo.New()
  server.Use(middleware.CORSWithConfig(middleware.CORSConfig{
   AllowOrigins: []string{"*"},
   AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
  }))
- general := server.Group("/general")
- {
-  general.GET("/hello", controller.Hello)
-  general.GET("/random", controller.Random)
- }
 
- server.Logger.Fatal(server.Start(":8081"))
+ server.GET("/", controller.Hello)
+ server.GET("/random", controller.Random)
+
+ server.Logger.Fatal(server.Start(":"+port))
 }
