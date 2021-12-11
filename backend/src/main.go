4010046c6a34cd8552f308a17f582955d8a95c56
Duc Trinh <huu-duc.trinh-luong@ekino.com>
2021-12-12 00:40:57 +0700

    fix(backend): Remove cors module as nginx was configured

--- backend/src/main.go
+++ backend/src/main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-    "github.com/gin-contrib/cors"
     "github.com/gin-gonic/gin"
     "k8s.io/apimachinery/pkg/util/rand"
 )
 
 func main() {
     router := gin.Default()
-
-    router.Use(cors.New(cors.Config{AllowAllOrigins: true}))
 
     router.GET("/api/strings/random", func(c *gin.Context) {
         c.JSON(200, gin.H{
