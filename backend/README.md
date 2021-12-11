a75d10de30eb3f431fd93a5b1c6ca841537b3af0
Duc Trinh <huu-duc.trinh-luong@ekino.com>
2021-12-12 01:23:31 +0700

    feat(doc): Update backend readme + integrate swagger

--- backend/README.md
+++ backend/README.md
@@ -1,1 +1,8 @@
-# hello-world
+# About
+Backend service built on top of Gin Web Framework.
+
+# API Swagger
+http://localhost/swagger/index.html
+
+# Demo
+http://212.144.240.35.bc.googleusercontent.com/swagger/index.html
--- backend/src/main.go
+++ backend/src/main.go
@@ -3,16 +3,32 @@ package main
 import (
     "github.com/gin-gonic/gin"
     "k8s.io/apimachinery/pkg/util/rand"
+    docs "github.com/huuduc2312/bootcamp-go/src/docs"
+    ginSwagger "github.com/swaggo/gin-swagger"
+    swaggerFiles "github.com/swaggo/files"
 )
+
+// @BasePath /api
+
+// @Summary Get a random string
+// @Description Get a random string
+// @Tags String
+// @Accept json
+// @Produce json
+// @Success 200 {string} a-random-string
+// @Router /strings/random [get]
+func getRandomStringfunc(c *gin.Context) {
+    c.JSON(200, gin.H{
+        "string": rand.String(20),
+    })
+}
 
 func main() {
     router := gin.Default()
-
-    router.GET("/api/strings/random", func(c *gin.Context) {
-        c.JSON(200, gin.H{
-            "string": rand.String(20),
-        })
-    })
-
+    docs.SwaggerInfo.BasePath = "/api"
+    
+    router.GET("/api/strings/random", getRandomStringfunc)
+    
+    router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
     router.Run()
 }
